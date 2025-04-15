9beb375d30ac32f0309e57512e4ee9ddeb70f6dc
yjymosheng <yjymosheng@gmail.com>
2025-04-15 10:39:43 +0800

    fix print.sh and add debug

--- scripts/test-print.sh
+++ scripts/test-print.sh
@@ -2,26 +2,46 @@
 
 cd arceos/ || exit
 
-# rm pflash.img -f 
-# rm disk.img -f
+rm pflash.img -f 
+rm disk.img -f
 
-# make pflash_img
-# make disk_img
+make pflash_img
+make disk_img
 
 
 make run A=exercises/print_with_color/ > a.txt 2>/dev/null
 
-output=$(grep -a "$(printf '\x1b')"  ./a.txt | grep "Hello, Arceos!"  ) || true
+context=$(tail -n 20 ./a.txt )
 
-# cat a.txt
-# echo "$output"
+has_color=false
+has_text=false
 
-rm a.txt -f
 
-if [[ -z "$output" ]] ;then
-    echo "print_with_color default"
-    exit 1
-else 
+if [[ "$context" == *$'\x1b['* ]]; then
+    echo "Has color"
+    has_color=true
+
+    sed_context=$(echo "$context" | sed 's/\x1b\[[0-9;]*m//g')
+
+    echo "$sed_context" > c.txt
+
+    if grep -q "Hello, Arceos!" c.txt ; then
+        echo "Has Hello, Arceos!"
+        has_text=true
+    else
+        echo "No Hello, Arceos!"
+    fi
+else
+    echo "No color"
+fi
+
+
+rm a.txt b.txt c.txt -f
+
+if [[ "$has_color" == true && "$has_text" == true ]]; then
     echo "print_with_color pass"
     exit 0
+else
+    echo "print_with_color fault"
+    exit 1
 fi
--- scripts/test-ramfs_rename.sh
+++ scripts/test-ramfs_rename.sh
@@ -18,12 +18,12 @@ make run A=exercises/ramfs_rename/ BLK=y > $tmp_file 2>/dev/null
 
 output=$(tail -n1 ./$tmp_file | grep -a "$grep_content")
 
-cat $tmp_file
+# cat $tmp_file
 
 rm -rf $tmp_file 
 
 if [[ -z "$output" ]] ;then
-    echo "ramfs_rename default"
+    echo "ramfs_rename fault"
     exit 1
 else 
     echo "ramfs_rename pass"
